6d047766e7dd1efc88e6f7ac74164bf5697ebaa8
jgroll97 <67149158+jgroll97@users.noreply.github.com>
2020-08-20 16:41:05 +0200

    Delte all files

--- config.py
+++
@@ -1,8 +0,0 @@
-INPUT_MODEL_DIR = "./input/model"
-INPUT_BACKGROUNDS_DIR = "./input/backgrounds"
-OUTPUT_DIR = "./output"
-OUTPUT_IMAGE_DIR = OUTPUT_DIR + "/images"
-OUTPUT_BOUNDINGBOX_DIR = OUTPUT_DIR + "/boundingbox"
-OUTPUT_MERGED_IMG = OUTPUT_DIR + "/merged_img"
-ROTATION_STEPS = 2
-NUM_LIGHTS = 1
